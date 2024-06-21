cdf4b85dfb2623d1306236275e47fa4b8864d421
nokcy <boryonok@gmail.com>
2024-06-21 12:52:30 +0100

    update

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ alert("Hello World")
 
  
  const subject = "JavaScript";
- const age = "23";
+ const age = 23;
 
  alert(age);
 
 
  console.log(age);
+
+ console.log(age+20);
+
+ console.log(typeof(subject));
