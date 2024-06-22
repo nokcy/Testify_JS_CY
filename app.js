17d730784b8bc2c7fd5a66c46c2dc5bd6faceeaa
nokcy <boryonok@gmail.com>
2024-06-22 12:55:11 +0100

    Odd and Even Number

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert("Hello World")
 
  
  const subject = "JavaScript";
+ const thisNumber = 67
  const age = 23;
 
  alert(age);
@@ -12,3 +13,34 @@ alert("Hello World")
  console.log(age+20);
 
  console.log(typeof(subject));
+ console.log(typeof(thisNumber));
+
+
+ console.log(4<=4);
+
+
+ const printMe = age===23 && thisNumber===607;
+
+ console.log (printMe);
+
+ const priprintThemntMe = age===23 || thisNumber===67;
+
+ console.log (priprintThemntMe);
+
+
+ if (age>=24) {
+    console.log("You are eligible to vote");
+    
+ } else {
+    console.log("gettat");
+    
+ }
+
+//Even and Odd Number
+const number = 74;
+ if (number % 2 === 0) {
+    console.log(number + " is an Even Number");
+    
+ } else {
+    console.log(number + " is an Odd Number");
+ }
